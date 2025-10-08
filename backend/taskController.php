66b6f6786faf8c2314e4929d91f781b004d6af4e
sd-eman <emanirfan06@gmail.com>
2025-10-08 09:11:43 +0200

    insert query

--- backend/taskController.php
+++ backend/taskController.php
@@ -16,6 +16,9 @@ if (empty($title)) {
     $errors[] = "Vul de afdeling in.";
 }
 $status= $_POST['status'];
+$deadline =$_POST['deadline'];
+$user = $_POST['user'];
+$created_at	= $_POST['created_at'];
 
 //1. Verbinding
 require_once '../../../backend/conn.php';
@@ -23,8 +26,8 @@ require_once '../../../backend/conn.php';
 
 
 //2. Query
-
-
+$query = "INSERT INTO taken (id, title, beschrijving, afdeling, status, deadline, user, created_at)
+VALUES (:id, :title, :beschrijving, :afdeling, :status, :deadline, :user, :created_at)"
 
 //3. Prepare
 ?>
