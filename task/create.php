94c947060fb55b7e3166739315da259e23b84a62
sd-eman <emanirfan06@gmail.com>
2025-10-10 11:03:29 +0200

    er was een kliene spel foutje in create.php ik heb het gefixt.

--- task/create.php
+++ task/create.php
@@ -13,22 +13,30 @@
 
         <form action="../backend/taskController.php" method="POST">
             <input type="hidden" name="action" value="create">
+            <div class="Taken">
+                <label for="titel">Titel: </label>
+                <input type="text" id="titel" name="titel" placeholder="Titel" required>
+            </div>
 
-            <label for="titel">Titel:</label>
-            <input type="text" id="titel" name="titel" required>
+            <div class="Taken">
+                <label for="afdeling">Afdeling: </label>
+                <input type="text" id="afdeling" name="afdeling" placeholder="Afdeling" required>
+            </div>
 
-            <label for="beschrijving">Beschrijving:</label>
-            <textarea id="beschrijving" name="beschrijving" required></textarea>
+            <div class="Taken">
+                <label for="beschrijving">Beschrijving: </label>
+                <textarea id="beschrijving" name="beschrijving" placeholder="Beschrijving" required></textarea>
+            </div>
 
-            <label for="afdeling">Afdeling:</label>
-            <input type="text" id="afdeling" name="afdeling" required>
-
-            <label for="status">Status:</label>
-            <select id="status" name="status" required>
-                <option value="todo">To Do</option>
-                <option value="doing">Doing</option>
-                <option value="done">Done</option>
-            </select>
+            <div class="Taken">
+                <label for="status">Status</label>
+                <select id="status" name="status" required>
+                    <option value="">-- Kies status --</option>
+                    <option value="todo">To Do</option>
+                    <option value="in-progress">In Progress</option>
+                    <option value="done">Done</option>
+                </select>
+            </div>
 
             <button type="submit">Taak toevoegen</button>
         </form>
